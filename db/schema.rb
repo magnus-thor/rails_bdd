fd899596acb67e5b47a2765adcc63e919e9392e9
magnus-thor <magnus.thor.ar@gmail.com>
2017-09-24 10:18:01 +0200

    rspec test to validate uniqueness of email passes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170923072518) do
+ActiveRecord::Schema.define(version: 20170924081553) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20170923072518) do
     t.bigint "article_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
     t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
