493338a0db2150e129aca8ef5dfa3f7649200409
magnus-thor <magnus.thor.ar@gmail.com>
2017-09-24 09:49:36 +0200

    created rspec test for uniqueness of email

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -6,10 +6,12 @@ RSpec.describe Comment, type: :model do
     it { is_expected.to have_db_column :commenter }
     it { is_expected.to have_db_column :body }
     it { is_expected.to have_db_column :article_id }
+    it { is_expected.to have_db_column :email }
   end
 
   describe 'Validations' do
     it { is_expected.to validate_presence_of :commenter }
     it { is_expected.to validate_presence_of :body }
+    it { is_expected.to validate_uniqueness_of :email }
   end
 end
