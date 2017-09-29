3ef3783a0f2ab9d08f9f59650dfc1976a6058c42
magnus-thor <magnus.thor.ar@gmail.com>
2017-09-29 13:22:29 +0200

    made changes to comment.rb to allow blank email

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,9 +2,5 @@ class Comment < ApplicationRecord
   belongs_to :article
   validates :commenter, presence: true, length: { minimum: 5}
   validates :body, presence: true
-  validates :email, presence: false
-=begin
-  validates :email, presence: false, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
-  try with blank
-=end
+  validates :email, presence: false, allow_blank: true, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
 end
